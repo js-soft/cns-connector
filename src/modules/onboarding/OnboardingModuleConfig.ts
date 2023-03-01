cb13ca62dfcadf7506749aa88d7414ed18959dfe
Korbinian Flietel <korbinian.flietel@js-soft.com>
2023-03-01 15:57:42 +0100

    chore: define onboarding specific config

---
+++ src/modules/onboarding/OnboardingModuleConfig.ts
@@ -0,0 +1,23 @@
+import { ConnectorRuntimeModuleConfiguration } from "../../ConnectorRuntimeModule";
+
+export interface OnboardingModuleConfig extends ConnectorRuntimeModuleConfiguration {
+    baseUrl: string;
+    realm: string;
+    client: string;
+    admin: {
+        username: string;
+        password: string;
+    };
+    passwordStrategy: "securePassword" | "randomPassword" | "ownPassword";
+    userIdStrategy: "enmeshedAddress" | "custom" | "relationshipId";
+    // The userData string list should contain the data that should be requested, if not allready present in the onboarding case, in enmeshed datatypes.
+    // The implementation of the IdentityProvider Interface that is being used is responsible for translating between the given enmeshed datatypes and the IDP datatypes
+    // Fields that cannot be parced to a enmeshed datatype should result in an error on startup since it is likely a configuration mistake which would leed
+    // to unexpected behaviour.
+    userData:
+        | {
+              req: string[] | undefined;
+              opt: string[] | undefined;
+          }
+        | undefined;
+}
