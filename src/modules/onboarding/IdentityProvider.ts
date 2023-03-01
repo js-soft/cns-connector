bb9b0205e7bc6cea26b95a32cb54aeda5e695cb5
Korbinian Flietel <korbinian.flietel@js-soft.com>
2023-03-01 15:57:27 +0100

    chore: define idp interface

---
+++ src/modules/onboarding/IdentityProvider.ts
@@ -0,0 +1,14 @@
+import { ResponseJSON } from "@nmshd/content";
+
+export interface IdentityProvider {
+    initialize(): Promise<void>;
+    onboard(change: ResponseJSON, userId: string): Promise<Result>;
+    register(change: ResponseJSON, userId: string, password: string): Promise<Result>;
+    getUser(userId: string): Promise<object | undefined>;
+    getExistingUserInfo(userId: string, requestedData: string[]): Promise<Map<string, string>>;
+}
+
+export enum Result {
+    Success,
+    Error
+}
